b95d1817298ae37e7cc51fe8eb6e9cec5f94a6e1
Adam Fudge <coolfudge@gmail.com>
2017-10-20 17:04:04 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,5 +1,7 @@
 class Cat
   attr_accessor :name, :mood
   def initialize(name, mood)
+    @name = name
+    @mood = mood
   end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,7 @@
 class Dog
   attr_accessor :name, :mood
   def initialize(name, mood)
+    @name = name
+    @mood = mood
   end
 end
